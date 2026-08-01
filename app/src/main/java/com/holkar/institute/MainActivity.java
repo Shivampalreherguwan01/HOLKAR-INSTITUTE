2b7ba33dfce7dab7b4d717c69120be0b9d6fd45b
Shivam Pal <palchanda467@gmail.com>
2026-08-01 12:43:09 +0530

    Update MainActivity to connect Login and Signup screens

---
+++ app/src/main/java/com/holkar/institute/MainActivity.java
@@ -0,0 +1,37 @@
+package com.holkar.institute;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnGoToLogin, btnGoToSignup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnGoToLogin = findViewById(R.id.btnGoToLogin);
+        btnGoToSignup = findViewById(R.id.btnGoToSignup);
+
+        btnGoToLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.class, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnGoToSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.class, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
